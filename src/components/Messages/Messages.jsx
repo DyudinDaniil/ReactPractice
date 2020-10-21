6e9a4755861eec0930053b35af3269f0606b9941
DyudinDaniil <ironboy778@gmail.com>
2020-10-21 20:28:54 +0300

    dialogs start

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -3,7 +3,43 @@ import messages from './Messages.module.css';
 
 const Messages = (props) => {
     return (
-        <div>Messages</div>
+        <div className={messages.content}>
+            <ul className={messages.nameUsers}>
+                <li className={messages.nameUsers__item}>
+                    Maria
+                </li>
+                <li className={messages.nameUsers__item}>
+                    Nick
+                </li>
+                <li className={messages.nameUsers__item}>
+                    Jhon
+                </li>
+                <li className={messages.nameUsers__item}>
+                    Daniil
+                </li>
+                <li className={messages.nameUsers__item}>
+                    Lara
+                </li>
+            </ul>
+
+            <div className={messages.dialogsWindow}>
+                <div className={messages.dialogsWindow__dialog}>
+                    welcome
+                </div>
+                <div className={messages.dialogsWindow__dialog}>
+                    hello
+                </div>
+                <div className={messages.dialogsWindow__dialog}>
+                    good
+                </div>
+                <div className={messages.dialogsWindow__dialog}>
+                    bye
+                </div>
+                <div className={messages.dialogsWindow__dialog}>
+                    luck
+                </div>
+            </div>
+        </div>
     );
 }
 
