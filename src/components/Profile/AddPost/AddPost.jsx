7e5ad7f0a0f09f3aea8642bed40d339fbc30167d
DyudinDaniil <DyudinDaniil2002@gmail.com>
2020-11-09 17:22:59 +0300

    Add func addPost

---
+++ src/components/Profile/AddPost/AddPost.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import profile from './AddPost.module.css';
+
+const AddPost = (props) => {
+    let newPostElement = React.createRef();
+
+    let addPost = () => {
+        let contentPost = newPostElement.current.value;
+        alert(contentPost);
+    }
+
+    return (
+        <div className={profile.content}>
+            <div className={profile.add}>
+                <a onClick={addPost} href="#" className={profile.add__link}>POST:</a>
+                <div className={profile.add__text}>
+                    <textarea ref={newPostElement}></textarea>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default AddPost;
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import profile from './Profile.module.css';
 import UserPosts from './UserPosts/UserPosts';
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
+import AddPost from "./AddPost/AddPost";
 
 const Profile = (props) => {
     return (
         <div className={profile.content}>
             <ProfileInfo />
+            <AddPost />
             <UserPosts postsData={props.state.postsData} />
         </div>
     );
