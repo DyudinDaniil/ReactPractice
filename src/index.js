d22c5895b6d89d7929dddbc5f48fa84cf71d3b68
DyudinDaniil <DyudinDaniil2002@gmail.com>
2020-11-28 13:51:14 +0300

    Add store in state.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import * as serviceWorker from './serviceWorker';
-import {renderEntireTree} from "./render";
-import state from "./redux/state";
+import store from "./redux/state";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App'
+import {BrowserRouter} from "react-router-dom";
 
-renderEntireTree(state);
+let renderEntireTree = (state) => {
+    ReactDOM.render(
+        <BrowserRouter>
+            <App state={store.getState()} addPost={store.addPost.bind(store)}
+                 updateNewPostText={store.updateNewPostText.bind(store)} />
+        </BrowserRouter>,
+        document.getElementById('root')
+    );
+}
+
+renderEntireTree(store.getState());
+store.subscribe(renderEntireTree);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,61 +1,67 @@
-import {renderEntireTree} from "../render";
-
-let state = {
-    profilePage: {
-        postsData: [
-            {id: 1, message: 'It is my first post', likesCount: 12},
-            {id: 2, message: 'How are you?', likesCount: 10},
-            {id: 3, message: 'Nice, thanks', likesCount: 19}
-        ],
-        newPostText: 'new post'
+let store = {
+    _state: {
+        profilePage: {
+            postsData: [
+                {id: 1, message: 'It is my first post', likesCount: 12},
+                {id: 2, message: 'How are you?', likesCount: 10},
+                {id: 3, message: 'Nice, thanks', likesCount: 19}
+            ],
+            newPostText: 'new post'
+        },
+        messagesPage: {
+            dialogsData: [
+                {id: 1, name: 'Maria'},
+                {id: 2, name: 'Nick'},
+                {id: 3, name: 'Jhon'},
+                {id: 4, name: 'Daniil'},
+                {id: 5, name: 'Vlad'},
+                {id: 6, name: 'Genry'},
+                {id: 7, name: 'Tom'},
+                {id: 8, name: 'Lera'},
+                {id: 9, name: 'Lara'}
+            ],
+            messagesData: [
+                {id: 1, message: 'Hi'},
+                {id: 2, message: 'hello'},
+                {id: 3, message: 'good'},
+                {id: 4, message: 'bye'},
+                {id: 5, message: 'luck'}
+            ]
+        },
+        teamsPage: {
+            teamsData: [
+                {id: 1, name: 'it-school', sub: 25, posts: 120},
+                {id: 2, name: 'new films', sub: 1178, posts: 1190},
+                {id: 3, name: 'GOTY', sub: 567, posts: 566},
+                {id: 4, name: 'library', sub: 208, posts: 240},
+                {id: 5, name: 'tutorials', sub: 157, posts: 435}
+            ]
+        }
     },
-    messagesPage: {
-        dialogsData: [
-            {id: 1, name: 'Maria'},
-            {id: 2, name: 'Nick'},
-            {id: 3, name: 'Jhon'},
-            {id: 4, name: 'Daniil'},
-            {id: 5, name: 'Vlad'},
-            {id: 6, name: 'Genry'},
-            {id: 7, name: 'Tom'},
-            {id: 8, name: 'Lera'},
-            {id: 9, name: 'Lara'}
-        ],
-        messagesData: [
-            {id: 1, message: 'Hi'},
-            {id: 2, message: 'hello'},
-            {id: 3, message: 'good'},
-            {id: 4, message: 'bye'},
-            {id: 5, message: 'luck'}
-        ]
+    getState() {
+        return this._state;
     },
-    teamsPage: {
-        teamsData: [
-            {id: 1, name: 'it-school', sub: 25, posts: 120},
-            {id: 2, name: 'new films', sub: 1178, posts: 1190},
-            {id: 3, name: 'GOTY', sub: 567, posts: 566},
-            {id: 4, name: 'library', sub: 208, posts: 240},
-            {id: 5, name: 'tutorials', sub: 157, posts: 435}
-        ]
+    _callSubscriber() {
+        console.log('State was changed');
+    },
+    addPost() {
+        let newPost = {
+            id: 4,
+            message: this._state.profilePage.newPostText,
+            likesCount: 12
+        }
+        this._state.profilePage.postsData.push(newPost);
+        this._state.profilePage.newPostText = '';
+        this._callSubscriber(this._state);
+    },
+    updateNewPostText(newText) {
+        this._state.profilePage.newPostText = newText;
+        this._callSubscriber(this._state);
+    },
+    subscribe(observer) {
+        this._callSubscriber = observer;
     }
 }
 
-window.state = state;
-
-export let addPost = () => {
-    let newPost = {
-        id: 4,
-        message: state.profilePage.newPostText,
-        likesCount: 12
-    }
-    state.profilePage.postsData.push(newPost);
-    state.profilePage.newPostText = '';
-    renderEntireTree(state);
-}
-
-export let updateNewPostText = (newText) => {
-    state.profilePage.newPostText = newText;
-    renderEntireTree(state);
-}
-
-export default state;
+export default store;
+window.store = store;
